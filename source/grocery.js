b35dbed84816944d83ea9e58978ebb505266b38f
Gunnari Auvinen & Danielle Sublett <pair+cheerazar.daniellesublett@devbootcamp.com>
2014-09-02 11:03:47 -0700

    Add jqueryui library and jquery script references in html. Add basic drag and drop functionality.

--- source/grocery.js
+++ source/grocery.js
@@ -3,3 +3,19 @@
  * What are their properties and methods?
  * How do your objects interact with their respective DOM elements?
  */
+$(document).ready( function () {
+
+  $('.item').draggable( {
+    helper: 'clone'
+  });
+
+  $('#grocery_list').droppable({
+    accept: '.item',
+    drop: function (event, ui) {
+      var itemTargetId = event.target.id;
+      var thingDragged = ui.draggable.context.outerHTML;
+      $('#' + itemTargetId).append(thingDragged);
+    }
+  });
+
+});
