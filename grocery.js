b86b9fe2fbef5afd7ea50f8de3f07313477adf7f
Danielle Sublett <danielle.sublett@gmail.com>
2014-10-25 15:28:30 -0700

    changes table size

--- grocery.js
+++ grocery.js
@@ -1,8 +1,4 @@
-/*
- * What are the objects in this exercise?
- * What are their properties and methods?
- * How do your objects interact with their respective DOM elements?
- */
+
 $(document).ready( function () {
   var opts = {
     dropSelector: '#grocery_list',
